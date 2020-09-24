5dd58f95303b02bf25b4b46451bea6d01965ec91
Stephan <stefapb81@gmail.com>
2020-09-24 14:11:16 +0200

    debut addBlocs mais voir pour la mise en forme

---
+++ View/php/addBlocs.php
@@ -0,0 +1,12 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Ajout de bloc pour étudiant</title>
+        <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    </head>
+    <body>
+
+    </body>
+</html>
