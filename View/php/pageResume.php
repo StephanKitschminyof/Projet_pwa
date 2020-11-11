4f88d7855873279e4ae6218e7423f3051865c6df
Stephan <stefapb81@gmail.com>
2020-11-11 19:02:54 +0100

    intégration de la pageResume plus changement du bottom menu

--- View/php/pageResume.php
+++ View/php/pageResume.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <?php
     include ("../../Controler/pageResumeControler.php");
+    include ("../../Controler/profilControler.php");
     $blocs = recupinfo();
     $prom = recuppromo();
 ?>
@@ -8,6 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php echo "Résumé " . " " . $_SESSION['prenom'] . " " . $_SESSION['nom']; ?></title>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
     <link rel="stylesheet" type="text/css" href="../css/bootstrap-reboot.min.css" />
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
     <link rel="stylesheet" type="text/css" href="../css/styleResume.css" />
@@ -17,19 +19,25 @@
 <body>
     <!-- HEADER : sticky, affiche le nom et la section de l'étudiant !! A CORRIGER !!-->
 	<header>
-        <div class="super_header">
-            <div class="header_bloc or">
-                <?php 
-                    echo "<p class=\"nom_etu\">".$_SESSION['nom']." ".$_SESSION['prenom']."</p>";
-                    echo "<p class=\"section\">".$prom['nom']."</p>";
-                ?>
-            </div>
-        </div>
+        <p id="profil-header-left"><?php echo substr($_SESSION['prenom'], 0, 1) . "." . $_SESSION['nom']; ?></p>
+        <p id="profil-header-right"><?php echo nomPromo(); ?></p>
     </header>
     <!-- CORPS : Accordion Bootstrap ; id en fonction de l'id du bloc-->
     <!-- DOC : https://getbootstrap.com/docs/4.0/components/collapse/ -->
     <?php
         createaccordion($blocs);
     ?>
+
+    <?php
+        include("./bottom_menu.php");
+    ?>
 </body>
+<script>
+        var color = <?php echo json_encode($_SESSION["couleurProfil"]);?>;
+        //Changement couleur pour les paragraphes
+        var p1 = document.getElementById("profil-header-left");
+        var p2 = document.getElementById("profil-header-right");
+        p1.style.color = color;
+        p2.style.color = color;
+    </script>
 </html>
